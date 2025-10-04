9801d4dc822eecfe916a39e7ece0d37a5bf16337
caesar <caesar@caesars-MacBook-Air.local>
2025-10-04 18:26:23 -0400

    took out nuxt icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/image', 'nuxt-icon'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
     // This line is required for Tailwind's styles to be included in your site.
     css: [
         '~/assets/css/main.css'
