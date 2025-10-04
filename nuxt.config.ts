3f86c165fdea49894d86f4160e5bc174c5918913
caesar <caesar@caesars-MacBook-Air.local>
2025-10-04 18:21:06 -0400

    made changes to nuxt and tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/image', 'nuxt-icon'],
+    // This line is required for Tailwind's styles to be included in your site.
+    css: [
+        '~/assets/css/main.css'
+    ]
 })
