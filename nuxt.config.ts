5d27e129766040164c1a188b070f69f1fa0a3831
caesar <caesar@caesars-MacBook-Air.local>
2025-10-06 19:02:25 -0400

    added changes nothing is working on upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
     srcDir: 'app/',
     modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
     // This line is required for Tailwind's styles to be included in your site.
