26803d28713991b6347478cc44c2950b12f92eeb
caesar <caesar@caesars-MacBook-Air.local>
2025-10-12 17:19:10 -0400

    took content our of tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./app/**/*.{js,vue,ts}",
-      "./app/components/**/*.{js,vue,ts}",
-      "./app/layouts/**/*.vue",
-      "./app/pages/**/*.vue",
-      "./app/plugins/**/*.{js,ts}",
-      "./app/app.vue",
-      ".app/error.vue",
-  ],
+
   theme: {
     extend: {
         colors: {
