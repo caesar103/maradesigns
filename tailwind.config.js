e35aa491bc23dcc3ce78ace469be8b1207be12ab
caesar <caesar@caesars-MacBook-Air.local>
2025-10-12 17:35:48 -0400

    Dropped tailwindcss from 4 to 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-
-  theme: {
-    extend: {
-        colors: {
-            'primary': '#2c3e50',
-            'secondary': '#ff7f50',
-            'accent': '#f1c40f',
+module.exports = {
+    content: [
+        "./app/**/*.{js,vue,ts}",
+        "./app/components/**/*.{js,vue,ts}",
+        "./app/layouts/**/*.vue",
+        "./app/pages/**/*.vue",
+        "./app/plugins/**/*.{js,ts}",
+        "./app/app.vue",
+        "./app/error.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#2c3e50',
+                'secondary': '#ff7f50',
+                'accent': '#f1c40f',
+            },
         },
-
     },
-  },
-  plugins: []
+    plugins: []
 }
-
